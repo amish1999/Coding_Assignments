8caf741f8953dca515d2f7cf9c235b1db056c6bd
amish1999 <47895005+amish1999@users.noreply.github.com>
2019-11-01 23:42:37 +0530

    Add files via upload

---
+++ add_One_to_number.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void incrementVector(vector<int> &a)
+{
+  int n = a.size();
+  a[n-1]+=1;
+  int carry = a[n-1]/10;
+  a[n-1] = a[n-1]%10;
+  for(int i=n-2;i>=0;i--)
+  {
+    if(carry==1)
+    {
+      a[i]+=1;
+      carry = a[i]/10;
+      a[i] = a[i]%10;
+
+    }
+  }
+  if(carry == 1)
+  a.insert(a.begin(),1);
+}
+int main() {
+  vector<int> vect{1,7,8,9};
+  incrementVector(vect);
+  for(int i=0;i<vect.size;i++)
+  cout<<vect[i]<<" ";
+  return 0;
+}
